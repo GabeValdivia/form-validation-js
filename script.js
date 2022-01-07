e0935102eb09df322327df2364a761ca7ec71a63
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-06 22:21:20 -0600

    Update event listener to check for valid email

--- script.js
+++ script.js
@@ -36,7 +36,10 @@ form.addEventListener('submit', function(e){
 	
 	if(email.value === ''){
 		showError(email, "Email is required");
-	} else {
+	} else if (!isValidEmail(email.value)) {
+		showError(email, "Email is not valid");
+	} 
+	else {
 		showSuccess(email);
 	}
 	
