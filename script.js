3840bd8c75392a74ae13d7829bc387f6571efd6f
GabeValdivia <gabe@gabeswebcoding.com>
2022-01-06 22:18:12 -0600

    Add JS method to validate email input using regex

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const password2 = document.getElementById('password2');
 function showError(input, message){
 	const formControl = input.parentElement;
 	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+//Check email is valid
+function isValidEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLocaleLowerCase());
 }
 
 //Event Listeners
@@ -19,4 +33,24 @@ form.addEventListener('submit', function(e){
 	} else {
 		showSuccess(username);
 	}
+	
+	if(email.value === ''){
+		showError(email, "Email is required");
+	} else {
+		showSuccess(email);
+	}
+	
+	if(password.value === ''){
+		showError(password, "Password is required");
+	} else {
+		showSuccess(password);
+	}
+	
+	if(password2.value === ''){
+		showError(password2, "Password2 is required");
+	} else {
+		showSuccess(password2);
+	}
+
+
 });
